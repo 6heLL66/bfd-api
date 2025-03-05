276c5436ea6f1a166456500a850c0fa47894d2cc
Никита Семенчук <n.semenchuk@texture.finance>
2025-03-05 16:50:36 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-// Load .env file
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:*', 'https://beraflow.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 

c01daeb146daa44b62734f1050be5038ad59db6d
Никита Семенчук <n.semenchuk@texture.finance>
2025-03-05 19:53:04 +0300

    asd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createClient } from '@supabase/supabase-js';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private supabase: SupabaseClient;
+  constructor(private readonly appService: AppService) {
+    this.supabase = createClient(
+      `https://${process.env.SUPABASE_PROJECT_ID}.supabase.co`,
+      process.env.SUPABASE_ANON_KEY!,
+    );
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('history')
+  async getHistory(): Promise<any> {
+    return (await this.supabase.from('treasure_history').select('*')).data;
+  }
+
+  @Get('tokens')
+  async getTokens(): Promise<any> {
+    return (await this.supabase.from('tokens').select('*')).data;
   }
 }
